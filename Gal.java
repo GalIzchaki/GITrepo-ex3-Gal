16bfa35d8e3f20f37c819de3d6ba391748f1e3c4
GalIzchaki <izchakigal@gmail.com>
2024-01-27 14:52:40 +0200

    updated Gal.java

--- Gal.java
+++ Gal.java
@@ -1,7 +1,7 @@
 public class Gal {
     public static void main(String[] args){
-        for(int i=1; i<=10; i++){
-            System.out.println("Gal");
+        for(int i=1; i<=3; i++){
+            System.out.println("DevOps");
         }
     }    
 }
